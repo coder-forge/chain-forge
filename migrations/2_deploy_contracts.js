2d15b6f2f3f6a0802d664883c241b4f3a53ac9b3
Daithi Coombes <webeire@gmail.com>
2017-04-05 19:00:26 +0100

    working on rospen testnetgit add .git add .

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -13,7 +13,7 @@ var CoderForge = artifacts.require('./CoderForge.sol');
 var Forge = artifacts.require('./Forge.sol');
 
 module.exports = function(deployer) {
-  //deployer.deploy(CoderForge);
-  //deployer.link(CoderForge, Forge);
+  deployer.deploy(CoderForge);
+  deployer.link(CoderForge, Forge);
   deployer.deploy(Forge);
 };
