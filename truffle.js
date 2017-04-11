345506459cafedaa63e19515f7af80869ea296b4
Daithi Coombes <webeire@gmail.com>
2017-04-11 23:18:54 +0100

    update instructions

--- truffle.js
+++ truffle.js
@@ -23,11 +23,7 @@ module.exports = {
       network_id: 3,        // Official Ethereum test network
       host: "localhost", // Random IP for example purposes (do not use)
       port: 8545,
-      from: "0x002d0d226Ec5d5926Cd0ddA186A055f6975b84b6"
-    },
-    "infura": {
-      network_id: 2,
-      host: "https://ropsten.infura.io/RozQs0Bj08iLTyFhwjVu",
+      from: ""
     },
     "staging": {
       network_id: 1337 // custom private network
